071ff539c2370c57c2d95b174e8023565a876702
heartwhite <abdullahakyurek@gmail.com>
2020-04-25 17:21:00 +0200

    unnecessary fragment removed

--- client/src/components/FilmItem.js
+++ client/src/components/FilmItem.js
@@ -5,16 +5,14 @@ import { Card, Image } from 'semantic-ui-react';
 export default function FilmItem({ film: { title, releaseDate, id }, posterUrl }) {
   const date = new Date(releaseDate).getFullYear();
   return (
-    <>
-      <Card href={`/film/${id}`}>
-        <Card.Content>
-          <Image floated='left' size='small' src={posterUrl} />
-          <Card.Header>
-            <h2>{title}</h2>
-          </Card.Header>
-          <Card.Meta>Released at {date}</Card.Meta>
-        </Card.Content>
-      </Card>
-    </>
+    <Card href={`/film/${id}`}>
+      <Card.Content>
+        <Image floated='left' size='small' src={posterUrl} />
+        <Card.Header>
+          <h2>{title}</h2>
+        </Card.Header>
+        <Card.Meta>Released at {date}</Card.Meta>
+      </Card.Content>
+    </Card>
   );
 }
